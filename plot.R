69c8932a23e68b3805509ac59f182c2365a523a2
Guillem Salazar <guillem.salazar@csic.es>
2026-05-05 16:00:22 +0200

    Add linear model

--- plot.R
+++ plot.R
@@ -1,8 +1,17 @@
 library(tidyverse)
+library(arm)
+library(easystats)
+library(visreg)
 
 dt<-penguins
 
+# Plot the data
 ggplot(data=dt,aes(x=body_mass,y=bill_len,color=species)) +
   geom_point() +
   geom_smooth(method="lm",se = F) +
   theme_bw()
+
+# Make an LM
+m1<-lm(bill_len~body_mass+species,data=dt)
+summary(m1)
+parameters(m1)
